e29c8e032631c37dbe109da739d4a8784afc1043
Evelyn <evelynoliveira.tds1.senai@gmail.com>
2025-04-24 14:08:42 -0300

    feat: create userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,66 @@
-//Controller for users
+const userModel = require("../models/userModel");
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await userModel.getUsers();
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(400).json({ message: "Erro ao buscar usuário" })
+    }
+};
+
+const getUserById = async (req, res) => {
+    try {
+        const user = await userModel.getUserById(req.params.id);
+
+
+        if (!user) {
+            res.status(404).json({ message: "Usuário não encontrado" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({ message: "Erro ao buscar usuário" })
+    }
+};
+
+const createUser = async (req, res) => {
+    try {
+        const { name, username, email location, following, followers } = req.body;
+        const photo = req.file ? req.file.filename : null;
+        const newUser = await userModel.createUser(name, username, email, location, following, followers, photo);
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.log(error);
+        if (error.code === "23505") {
+            res.status(400).json({ message: "Email já cadastrado" });
+        }
+        res.status(404).json({ message: "Erro ao criar usuário" });
+    }
+};
+
+
+const updateUser = async (req, res) => {
+    try {
+        const { name, username, email, location } = req.body
+        const photo = req.file ? req.file.filename : null;
+        const updateUser = await userModel.updateUser(req.params.id, name, username, email, location, photo );
+
+        if(!updateUser) {
+            res.status(404).json({ message: "Usuário não encontrado" });
+        }
+    } catch (error) {
+            res.status(404).json({ message: "Erro ao atualizar usuário" });
+
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const message = await userModel.deletarUser(req.params.id);
+        res.json(message);
+    } catch (error) {
+        res.status(404).json({ message: "Erro ao deletar usuário" });
+    }
+}
+
+module.exports = { getAllUsers, getUserById, createUser, updateUser, deleteUser }
