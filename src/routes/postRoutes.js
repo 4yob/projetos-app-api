dc47380b2d71c68486f119d0804c496557a7584e
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-05-13 10:21:09 -0300

    Refactor: swagger fix

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -8,14 +8,14 @@ const upload = multer({ dest: "uploads/" });
  * @swagger
  * tags:
  *   name: Posts
- *   description: Gerenciamento das rotas de posts
+ *   description: Gerenciamento de Posts
  */
 
 /**
  * @swagger
  * /api/posts/{id}:
  *   get:
- *     summary: Retorna um post específico
+ *     summary: Busca um Post por ID
  *     tags: [Posts]
  *     parameters:
  *       - in: path
@@ -23,10 +23,10 @@ const upload = multer({ dest: "uploads/" });
  *         required: true
  *         schema:
  *           type: integer
- *         description: ID do post
+ *         description: ID do Post
  *     responses:
  *       200:
- *         description: Post encontrado com sucesso
+ *         description: Post encontrado
  *       404:
  *         description: Post não encontrado
  */
@@ -54,7 +54,7 @@ router.get("/posts", postController.getPosts);
  * @swagger
  * /api/posts:
  *   post:
- *     summary: Cria um novo post
+ *     summary: Criar um novo Post
  *     tags: [Posts]
  *     requestBody:
  *       required: true
@@ -63,21 +63,24 @@ router.get("/posts", postController.getPosts);
  *           schema:
  *             type: object
  *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome do Post
  *               title:
  *                 type: string
- *                 description: Título do post
+ *                 description: Título do Post
  *               content:
  *                 type: string
- *                 description: Conteúdo do post
+ *                 description: Descrição do Post
  *               photo:
  *                 type: string
  *                 format: binary
- *                 description: Imagem do post
+ *                 description: Foto do Post
  *     responses:
  *       201:
  *         description: Post criado com sucesso
  *       400:
- *         description: Erro ao criar o post
+ *         description: Requisição inválida
  */
 router.post("/posts", upload.single("photo"), postController.createPost);
 
@@ -85,7 +88,7 @@ router.post("/posts", upload.single("photo"), postController.createPost);
  * @swagger
  * /api/posts/{id}:
  *   put:
- *     summary: Atualiza um post existente
+ *     summary: Atualiza um Post existente
  *     tags: [Posts]
  *     parameters:
  *       - in: path
@@ -97,20 +100,34 @@ router.post("/posts", upload.single("photo"), postController.createPost);
  *     requestBody:
  *       required: true
  *       content:
- *         multipart/form-data:
+ *         application/json:
  *           schema:
  *             type: object
  *             properties:
  *               title:
  *                 type: string
+ *                 description: Título do post
  *               content:
  *                 type: string
+ *                 description: Descrição do post
  *               photo:
  *                 type: string
  *                 format: binary
+ *                 description: Foto do post
+ *               categoria:
+ *                 type: string
+ *                 description: Categoria do post
+ *               likes:
+ *                 type: integer
+ *                 description: Número de likes do post
+ *               comments:
+ *                 type: integer
+ *                 description: Número de comentários do post
  *     responses:
  *       200:
  *         description: Post atualizado com sucesso
+ *       400:
+ *         description: Requisição inválida
  *       404:
  *         description: Post não encontrado
  */
@@ -120,7 +137,7 @@ router.put("/posts/:id", postController.updatePost);
  * @swagger
  * /api/posts/{id}:
  *   delete:
- *     summary: Deleta um post existente
+ *     summary: Deleta um Post existente
  *     tags: [Posts]
  *     parameters:
  *       - in: path
@@ -139,26 +156,59 @@ router.delete("/posts/:id", postController.deletePost);
 
 /**
  * @swagger
- * /api/posts/like/{postId}:
- *   post:
- *     summary: Adiciona um like a um post
+ * /api/likes/{id}/likes:
+ *   patch:
+ *     summary: Atualizar o número de curtidas de um post
  *     tags: [Posts]
  *     parameters:
  *       - in: path
- *         name: postId
+ *         name: id
  *         required: true
  *         schema:
  *           type: integer
- *         description: ID do post a ser curtido
+ *         description: ID do Post
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               action:
+ *                 type: string
+ *                 enum: [like, unlike]
+ *                 description: Ação para atualizar curtidas
  *     responses:
  *       200:
- *         description: Like adicionado com sucesso
+ * 
+ *         description: Seguidores atualizados com sucesso
+ *       400:
+ *         description: Ação inválida
  *       404:
  *         description: Post não encontrado
  */
-
 router.patch("/posts/:id/likes", postController.updateLikes);
 
+
+/**
+ * @swagger
+ * /api/posts/{id}/likes:
+ *   get:
+ *     summary: Obter o número de curtidas de um post
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do Post
+ *     responses:
+ *       200:
+ *         description: Número de Curtidas retornado com sucesso
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.get("/posts/:id/likes", postController.getLikes);
 
 
